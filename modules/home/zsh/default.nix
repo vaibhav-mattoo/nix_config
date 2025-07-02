32a01a751c366d6c3d527d0fae7f748015ac3b48
vaibhav <vaibhavmattoo1@gmail.com>
2025-07-02 15:15:15 -0500

    added aliases
    Signed-off-by: vaibhav <vaibhavmattoo1@gmail.com>

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -60,6 +60,13 @@
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       cat = "bat";
       man = "batman";
+      lg = "lazygit";
+      :q = "exit";
+      :Q = "exit";
+      cp = "cp -iv";
+      mv = "mv -iv";
+      rm = "rm -iv";
+      mkdir = "mkdir -pv";
     };
   };
 }
